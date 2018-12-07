509ddf0ef72b026f30615dc34ad879aae87ff421
Alexandru Tonu <tonu.alexandru99@gmail.com>
2018-12-07 17:06:30 +0200

    new branch

--- IPC/main.c
+++ IPC/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+void addValues(int value) {
+    printf("%d", value);
+}
+
 int main() {
-    printf("Hello world");
+    int option = 1;
+    do {
+        printf("Hello world");
+        scanf("%d", & option);
+
+        switch (option) {
+            case 1:
+                addValues();
+                break;
+            case 0: 
+                quitExecution();
+                break;
+            default:
+                printf("Please insert a valid value");
+        }
+    } while (option != 0);
+
     return 0;
 }
