fc67a0ab7188331ff22d683b37eaf77985dba41a
TONUAlexandru <tonu.alexandru99@gmail.com>
2019-01-21 14:32:33 +0200

    feat: remove unnecessary pointers

--- IPC/practice.c
+++ IPC/practice.c
@@ -34,49 +34,49 @@ void showOff(store * Darwin, int counter) {
         Darwin[counter].price
     );
 }
-void search(store *Darwin, int *counter, char _model[20]){
+void search(store * Darwin, int counter, char _model[20]) {
     int i;
-    for (i=0; i<=(*counter); i++) {
-        if ( strcmp(Darwin[i].model, _model) == 0) {
+    for (i = 0; i <= (counter); i++) {
+        if (strcmp(Darwin[i].model, _model) == 0) {
             showOff(Darwin, i);
         }
     }
 }
-void compare(store *Darwin, int *counter, int price){
+void compare(store * Darwin, int counter, int price) {
     int i;
-    for (i=0; i<=(*counter); i++) {
+    for (i = 0; i <= (counter); i++) {
         if (Darwin[i].price < price) {
             showOff(Darwin, i);
         }
     }
 }
-void sort (store *Darwin, int *counter) {
+void sort(store * Darwin, int counter) {
     int i, sorted;
     store clone;
     do {
         sorted = 1;
-        for (i=0; i<(*counter); i++){
-            if (strcmp (Darwin[i].model, Darwin[i+1].model) > 0) {
+        for (i = 0; i < (counter); i++) {
+            if (strcmp(Darwin[i].model, Darwin[i + 1].model) > 0) {
                 sorted = 0;
                 clone = Darwin[i];
-                Darwin[i] = Darwin[i+1];
-                Darwin[i+1] = clone;
+                Darwin[i] = Darwin[i + 1];
+                Darwin[i + 1] = clone;
             }
         }
     } while (!sorted);
 }
 
-void delete (store *Darwin, int *counter, char _model[20]) {
+void delete(store * Darwin, int * counter, char _model[20]) {
     int i, j, deleted = 0;
-    for (i=0; i<=(*counter - deleted); i++){
-        if (strcmp (Darwin[i].model, _model) == 0) {
+    for (i = 0; i <= ( * counter - deleted); i++) {
+        if (strcmp(Darwin[i].model, _model) == 0) {
             deleted++;
-            for (j=i; j< (*counter - deleted); j++){
-                Darwin[j] = Darwin[j+1];
+            for (j = i; j < ( * counter - deleted); j++) {
+                Darwin[j] = Darwin[j + 1];
             }
             i--;
         }
-        *counter = *counter - deleted;
+        * counter = * counter - deleted;
     }
 }
 
@@ -115,25 +115,25 @@ int main(void) {
                 break;
             case 3:
                 printf("\nDesired model: ");
-                while(getchar() != '\n')
+                while (getchar() != '\n')
                 ;
                 gets(_model);
-                search(Darwin, &n, _model);
+                search(Darwin, n, _model);
                 break;
             case 4:
                 printf("\nModels cheaper than: ");
-                scanf("%d", &_price);
-                compare(Darwin, &n, _price);
+                scanf("%d", & _price);
+                compare(Darwin, n, _price);
                 break;
             case 5:
-                sort(Darwin, &n);
+                sort(Darwin, n);
                 break;
             case 6:
                 printf("\nModels to remove: ");
-                while(getchar() != '\n')
+                while (getchar() != '\n')
                 ;
                 gets(_model);
-                delete(Darwin, &n, _model);
+                delete(Darwin, & n, _model);
                 break;
             default:
                 printf("\nInvalid option");
