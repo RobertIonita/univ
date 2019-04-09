122e8af4a1ce8699a31508fe87e8d634e090d11b
tonualexandru <tonu.alexandru99@gmail.com>
2019-04-09 23:18:10 +0300

    feat: refactor + item1 test

--- SDA/TEST/item1.c
+++ SDA/TEST/item1.c
@@ -4,108 +4,107 @@
 
 #define MAX 30
 
-typedef struct list {
+typedef struct item {
+    char data[MAX];
+    struct item * next;
+} node;
+
+typedef struct  {
     char str[MAX];
-    struct list * next;
-}
-node;
+} preadd;
 
-node * root = NULL;
+preadd items[100];
 
-void *add (node *first, char * str) {
-    node *q1, *q2, *clone;
+node * add (node * first, char * str) {
+    node *q1, *q2,
+        *clone;
     clone = (node *)malloc(sizeof(node));
-    strcpy(clone -> str, str);
+    strcpy(clone -> data, str);
     clone -> next = NULL;
 
-    for(q1 = q2 = first; q1!=NULL && (strcmp(q1 -> str, clone -> str) < 0); q2=q1, q1=q1->next);
+    for(q1 = q2 = first; q1 != NULL && (strcmp(q1 -> data, clone -> data) < 0); q2 = q1, q1 = q1 -> next);
     if (q1 == q2) {
         clone -> next = first;
         first = clone;
     } else {
         q2 -> next = clone;
-        clone->next = q1;
+        clone -> next = q1;
     }
     return first;
 }
-void show(node * first) {
-    node * q;
-    for (q = first; q != NULL; q = q-> next) {
-        printf("%s\n", q -> str);
-    }
-}
-
-void complement(node * first) {
-    node * q;
-    int i;
-    for (q = first; q != NULL; q = q-> next) {
-        for(i = 0; (q -> str)[i] != '\0'; i++) {
-            if ((q -> str)[i] == '0') {
-                printf("1");
-            } else {
-                printf("0");
-            }
-        }
-        printf("\n");
-    }
-}
-
-void max(node * first) {
-    node * q;
-    int i,
-        k = 0,
-        j= 0,
-        n = 1;
-    char reg[MAX+1];
-    strcpy(reg, "");
-    for (q = first; q != NULL; q = q-> next, j++) {
-        k = 0;
-        for(i = 0; (q -> str)[i] != '\0'; i++) {
-            if ((q -> str)[i] == '1') {
-                k++;
-            }
-        }
-        if (reg[0] < '0'+k){
-            n = 1;
-            reg[0] = '0'+k;
-            reg[1] = '0'+j;
-            reg[2] = '\0';
-        } else if( reg[0] == '0'+k) {
-            n++;
-            reg[n] = '0'+j;
-        }
-        reg[n+1] = '\0';
-    }
-    for(i = 1; reg[i] != '\0'; i++) {
-        n = 0;
-        q = first;
-        while(reg[i]-48 > n++ && q != NULL){
-            q = q->next;
-        }
-        
-        printf("\nindices: %c %s", reg[i], q->str);
-    }
-    printf("\nmax: %c", reg[0]);
-}
-
-void read() {
-    char str[MAX];
-    FILE * f;
-    if ((f = fopen("SDA/TEST/binary.txt", "rt")) == NULL) {
-        printf("error");
+node * read(node * item, char * path) {
+    int i = 0,
+        j, count = 0;
+    FILE *f;
+    if( (f = fopen(path, "rt") ) == NULL) {
+        printf("\nerror reading file");
     } else {
         while (!feof(f)) {
-            fscanf(f, "%s", str);
-            root = add(root, str);
+            fscanf(f, "%s", items[i].str);
+            count=0;
+            for(j = 0; j <i; j++) {
+                if ( strcmp(items[j].str, items[i].str) == 0) {
+                    count++;
+                }
+            }
+            if (count == 0)
+                item = add(item, items[i].str);
+            i++;
         }
         fclose(f);
     }
+    return item;
 }
 
+node * root = NULL;
+
+void showOff(node * first) {
+    node * q;
+    for (q = first; q != NULL; q = q -> next) {
+        printf("\n%s", q -> data);
+    }
+}
+
+
+node * deleteItem (node * item, char * desired) {
+    node *q1, *q2;
+    for (q1 = q2 = item; q1 != NULL && strcmp (q1 -> data, desired); q2 = q1, q1 = q1 -> next);
+
+    if (q1 != NULL && (strcmp (q1 -> data, desired) == 0) ) {
+        if (q1 == q2) {
+            item = item -> next;
+        } else {
+            q2 -> next = q1 -> next;
+            free(q1);
+        }
+    }
+    return item;
+}
+
+void search (node * item) {
+    int len;
+    printf("\nInsert desired length: ");
+    scanf("%d", &len);
+    node *q;
+    for (q = item; q != NULL; q = q -> next) {
+        if (strlen(q -> data) == len) {
+            printf("\n%s", q -> data);
+        }
+    }
+}
+
+
 int main() {
-    read();
-    show(root);
-    // complement(root);
-    max(root);
+    char desired[MAX];
+    int nr, i;
+
+    root = read (root, "SDA/TEST/arta_listelor1.txt");
+    
+    showOff(root);
+    printf("\nInsert record to be deleted: ");
+    scanf("%s", desired);
+    root = deleteItem(root, desired);
+    showOff(root);
+    search(root);
     return 0;
 }
---
+++ SDA/TEST/item3.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 30
+
+typedef struct list {
+    char str[MAX];
+    struct list * next;
+}
+node;
+
+node * root = NULL;
+
+void *add (node *first, char * str) {
+    node *q1, *q2, *clone;
+    clone = (node *)malloc(sizeof(node));
+    strcpy(clone -> str, str);
+    clone -> next = NULL;
+
+    for(q1 = q2 = first; q1!=NULL && (strcmp(q1 -> str, clone -> str) < 0); q2=q1, q1=q1->next);
+    if (q1 == q2) {
+        clone -> next = first;
+        first = clone;
+    } else {
+        q2 -> next = clone;
+        clone->next = q1;
+    }
+    return first;
+}
+void show(node * first) {
+    node * q;
+    for (q = first; q != NULL; q = q-> next) {
+        printf("%s\n", q -> str);
+    }
+}
+
+void complement(node * first) {
+    node * q;
+    int i;
+    for (q = first; q != NULL; q = q-> next) {
+        for(i = 0; (q -> str)[i] != '\0'; i++) {
+            if ((q -> str)[i] == '0') {
+                printf("1");
+            } else {
+                printf("0");
+            }
+        }
+        printf("\n");
+    }
+}
+
+void max(node * first) {
+    node * q;
+    int i,
+        k = 0,
+        j= 0,
+        n = 1;
+    char reg[MAX+1];
+    strcpy(reg, "");
+    for (q = first; q != NULL; q = q-> next, j++) {
+        k = 0;
+        for(i = 0; (q -> str)[i] != '\0'; i++) {
+            if ((q -> str)[i] == '1') {
+                k++;
+            }
+        }
+        if (reg[0] < '0'+k){
+            n = 1;
+            reg[0] = '0'+k;
+            reg[1] = '0'+j;
+            reg[2] = '\0';
+        } else if( reg[0] == '0'+k) {
+            n++;
+            reg[n] = '0'+j;
+        }
+        reg[n+1] = '\0';
+    }
+    for(i = 1; reg[i] != '\0'; i++) {
+        n = 0;
+        q = first;
+        while(reg[i]-48 > n++ && q != NULL){
+            q = q->next;
+        }
+        
+        printf("\nindices: %c %s", reg[i], q->str);
+    }
+    printf("\nmax: %c", reg[0]);
+}
+
+void read() {
+    char str[MAX];
+    FILE * f;
+    if ((f = fopen("SDA/TEST/binary.txt", "rt")) == NULL) {
+        printf("error");
+    } else {
+        while (!feof(f)) {
+            fscanf(f, "%s", str);
+            root = add(root, str);
+        }
+        fclose(f);
+    }
+}
+
+int main() {
+    read();
+    show(root);
+    // complement(root);
+    max(root);
+    return 0;
+}
