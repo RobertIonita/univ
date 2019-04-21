2b8459528012063fa4e34a4d83e462344c2be311
tonualexandru <tonu.alexandru99@gmail.com>
2019-04-21 12:58:35 +0300

    feat: performed one dded

--- SDA/Practice/bin_search.c
+++ SDA/Practice/bin_search.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #define MAX 9
 
+int binPerformed(int x, int n, int (*arr)[n])
+{
+    int s = 0,           // search field start
+        e = n - 1,       // search field end
+        m; // search field middle
+    do
+    {
+        m = (s + e) / 2;
+        (*arr)[m] < x ? (s = m + 1) : (e = m);  // magic
+
+    } while (s < e); // while search field is not empty
+    return (*arr)[e] == x ? e : -1;     // if x was found return its index in the array, otherwise -1
+}
+
 int binSearch(int x, int n, int (*arr)[n])
 {
     int s = 0,           // search field start
@@ -17,10 +31,14 @@ int binSearch(int x, int n, int (*arr)[n])
 
 int main()
 {
-    int arr[MAX] = {3, 6, 8, 11, 34, 34, 90, 92, 1001},
+    int arr[MAX] = {0, 1, 2, 3, 4, 5, 6, 7, 8},
         x;
-    printf("\nsearch: ");
-    scanf("%d", &x);
-    printf("\nindex: %d", binSearch(x, MAX, &arr));
+    do {
+        printf("\nsearch: ");
+        scanf("%d", &x);
+        printf("\nindex: %d", binSearch(x, MAX, &arr));
+        printf("\nindex: %d", binPerformed(x, MAX, &arr));
+    } while (x != -1);
+    
     return 0;
 }
