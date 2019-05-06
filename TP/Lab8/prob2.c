7abc2188fcbd476914b16eb6089ec4e201aeed3d
TONUAlexandru <tonu.alexandru99@gmail.com>
2019-05-06 08:50:29 +0300

    Labs updated

---
+++ TP/Lab8/prob2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void citeste(char ** nume, int * varsta) {
+    *nume = (char *) malloc(4 * sizeof(char));
+    printf("Introduceti numele: ");
+    scanf("%s", *nume);
+    printf("Introduceti varsta: ");
+    scanf("%d", &(*varsta));
+}
+void scrie(char * nume, int varsta) {
+    printf("%s,%d\n", nume, varsta);
+}
+int main(void) {
+    char * nume;
+    int varsta;
+    citeste( & nume, & varsta);
+    scrie(nume, varsta);
+    free(nume);
+
+    return 0;
+}
