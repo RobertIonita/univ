29cba15d7432ee78c0e7b5bc523d00ddc3e304ff
tonualexandru <tonu.alexandru99@gmail.com>
2019-05-16 22:51:37 +0300

    refactor: by myself

--- TP/Lab7/Proposed/item4.c
+++ TP/Lab7/Proposed/item4.c
@@ -1,24 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void init (int *N, int *n) {
-	N = (int *) malloc (*n * sizeof(int));
-	if(!N) {
+void init (int **N, int n) {
+	*N = (int *) malloc (n * sizeof(int));
+	if(!*N) {
 		printf("Can not allocate");
 		exit(EXIT_FAILURE);
 	}
 }
-
-void populate(int *N, int *n) {
-	printf("%d", sizeof(*N));
-	*n = 0;
+void update (int **N, int n, int x) {
+	int *try;
+	try = (int *) realloc (*N, n * sizeof(int));
+	if(!try) {
+		printf("Can not reallocate");
+		exit(EXIT_FAILURE);
+	} else {
+		**N = *try;
+		(*N)[n-1] = x;
+	}
 }
 
+void populate(int **N, int n, int *x) {
+	printf("Insert Nr: ");
+	scanf("%d", &(*x));
+	update(&(*N), n, *x);
+}
+
+void showOff(int *N, int n) {
+	for (int i = 0; i < n; i++)
+		printf("%d ", N[i]);
+}
 int main () {
-	int N, n = 320;
-	init(&N, &n);
-	while (n !=0 )
-		populate(&N, &n);
-	
+	int *N = NULL,
+		n = 0,
+		x = !0;
+	init(&N, n);
+	while (x != 0)
+		populate(&N, ++n, &x);
+	showOff(N, n);
+	free(N);
 	return 0;
 }
