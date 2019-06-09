d4d7db28a56647ca138df01a4e06e4955127dca9
tonualexandru <tonu.alexandru99@gmail.com>
2019-06-10 02:19:40 +0300

    clean start

--- TP/Lab7/Proposed/item5.c
+++ TP/Lab7/Proposed/item5.c
@@ -1,55 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_STR_LEN 80
+#define MAX 20
 
-void shift(char *str, int times)
+void split(int length, char (*token)[length], char *str, char separator)
 {
-    int i = 0;
-    for (i = 0; str[i + times] != '\0'; i++)
-        str[i] = str[i + times];
-    str[i] = '\0';
-}
-
-char *trim(char *str, char ch)
-{
-    int i;
-    for (i = 0; str[i] != '\0'; i++)
-        ;
-    while (str[--i] == ch)
-        str[i] = '\0';
-    i = -1;
-    while (str[++i] == ch)
-        ;
-    shift(str, i);
-    return str;
-}
-
-void split(char (*clone)[3], char *str, char separator) {
     int j = 0, k = 0, i = 0;
     for (i = 0; str[i] != '\0'; i++)
     {
-        if (str[i] != separator) {
-            clone[k][j++] = str[i];
-        } else {
-            clone[k][j] = '\0';
-            k++; j = 0;
+        if (str[i] != separator)
+        {
+            token[k][j++] = str[i];
+        }
+        else
+        {
+            token[k][j] = '\0';
+            k++;
+            j = 0;
         }
     }
-    for (int i = 0; i < 4; i++)
-        printf("\n[%d]: %s", i, clone[i] );
 }
 
-void read (char *str, char (*arr)[3]) {
-    scanf("%[^\n]s", str);
-    split(arr, str, ' ');
-    printf("%s", str);
+void dynamicRead(char **str, FILE * terminal, char finish)
+{
+    char ch;
+    unsigned long long
+        size = 1,
+        index = 0;
+    *str = (char*) malloc(sizeof(char) * size);
+    while ((ch = fgetc(terminal)) != EOF && ch != finish)
+    {
+        (*str)[index++] = ch;
+        if (index == size)
+        {
+            (*str) = realloc((*str), sizeof(char) * (size += 1));
+        }
+    }
+    (*str)[index++] = '\0';
+    printf("\n%llu", index);
 }
 
-int main () {
+int main(void)
+{
     char *input,
-        arr[4][3];
-    read(&input, arr);
+        token[4][MAX];
+    FILE *f;
+    f = fopen("TP/Lab7/Proposed/test.txt", "r");
+    printf("input string : ");
+    dynamicRead(&input, f, '#');
+    // printf("%s\n", input);
 
+    free(input);
     return 0;
 }
