9c4f979875abfea104cada11322dc920a777ee78
tonualexandru <tonu.alexandru99@gmail.com>
2019-06-11 22:59:21 +0300

    handle empty line at eof

--- TP/Teo_cries/EXAMEN/ex2_2.c
+++ TP/Teo_cries/EXAMEN/ex2_2.c
@@ -7,8 +7,8 @@
 void citire()
 {
     FILE *f, *g;
-    char sir[MAX];
-    int n, k = 0;
+    char sir[MAX], ch;
+    int n, k = 0, lines = 1;
     f = fopen("TP/Teo_cries/EXAMEN/assets/ex2_2.txt", "rt");
     g = fopen("TP/Teo_cries/EXAMEN/assets/ex2_2_iesire.txt", "wt");
     if (!f || !g)
@@ -17,6 +17,13 @@ void citire()
     }
     while (!feof(f))
     {
+        fscanf(f, "%c", &ch);
+        if (ch == '\n')
+            lines++;
+    }
+    rewind(f);
+    while (lines--)
+    {
         fscanf(f, "%s", sir);
         n = strlen(sir);
         for (size_t i = 0; sir[i] != '\0'; i++)
@@ -24,12 +31,11 @@ void citire()
             if (sir[i] >= '0' && sir[i] <= '9')
                 k++;
         }
-        if (!k) {
-            fprintf(g, " %s", sir);
-        }
+        if (!k)
+            fprintf(g, "\n%s", sir);
         k = 0;
     }
-
+        
     fclose(g);
     fclose(f);
 }
@@ -39,9 +45,7 @@ void afisare()
     char sir[MAX];
     f = fopen("TP/Teo_cries/EXAMEN/assets/ex2_2_iesire.txt", "rt");
     if (!f)
-    {
         exit(EXIT_FAILURE);
-    }
     while (!feof(f))
     {
         fscanf(f, "%s", sir);
