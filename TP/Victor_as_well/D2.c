21dba02ab3b7799011fccd76ee7e55ecb2294cf9
tonualexandru <tonu.alexandru99@gmail.com>
2019-06-26 23:26:22 +0300

    WIP

--- TP/Victor_as_well/D2.c
+++ TP/Victor_as_well/D2.c
@@ -7,12 +7,6 @@ FILE *binary_file;
 
 struct
 {
-
-	char titlu[30];
-	char autor[20];
-	int nr_pag : 6;
-	int cr : 1;	//Carte sau revista
-	char ISBN[13]; //ISBN pentru carte
 	int anul;	  //Anul aparitiei revistei
 } t[10];
 
@@ -20,63 +14,23 @@ void Adaugare()
 {
 
 	int aux;
-
-	while (getchar() != '\n')
-		;
-	printf("Titlul cartii: ");
-	gets(t[pos].titlu);
-	printf("Autorul cartii: ");
-	gets(t[pos].autor);
-	printf("Numarul de pagini: ");
-	scanf("%d", &aux);
-	t[pos].nr_pag = aux;
-	printf("Carte sau revista? ");
-	scanf("%d", &aux);
-	t[pos].cr = aux;
-	if (aux == 0)
-	{
-		printf("ISBN-ul cartii: ");
-		scanf("%s", t[pos].ISBN);
-	}
-	else
-	{
-		printf("Anul aparitiei revistei: ");
-		scanf("%d", &t[pos].anul);
-	}
+	printf("Anul aparitiei revistei: ");
+	scanf("%d", &t[pos].anul);
 	pos++;
-	printf("\n");
 }
 
 void Afisare()
 {
-
 	int aux;
-
-	for (int index = 0; index < pos; index++)
+	for (int i = 0; i < pos; i++)
 	{
-		printf("Titlul cartii: ");
-		puts(t[index].titlu);
-		fwrite(t[index].titlu, strlen(t[index].titlu), 1, binary_file);
-		printf("Autorul cartii: ");
-		puts(t[index].autor);
-		fwrite(t[index].autor, strlen(t[index].autor), 1, binary_file);
-		printf("Numarul de pagini: %d\n", t[index].nr_pag);
-		aux = t[index].nr_pag;
-		fwrite(&aux, 1, 1, binary_file);
-		if (t[index].cr == 0)
-		{
-			printf("ISBN-ul cartii: %s\n", t[index].ISBN);
-			fwrite(t[index].ISBN, 4, 1, binary_file);
-		}
-		else
-		{
-			printf("Anul aparitiei revistei: %d\n", t[index].anul);
-			aux = t[index].anul;
-			fwrite(&aux, 4, 1, binary_file);
-		}
+		printf("Anul: %d\n", t[i].anul);
+		aux = t[i].anul;
+		fwrite(&aux, 4, 1, binary_file);
 		printf("\n");
 	}
 }
+void showOff(char *path);
 
 int main(int argc, char *argv[])
 {
@@ -85,5 +39,20 @@ int main(int argc, char *argv[])
 	//Adaugare();
 	binary_file = fopen(argv[1], "wb");
 	Afisare();
+	showOff(argv[1]);
 	fclose(binary_file);
+}
+
+void showOff(char *path) {
+    FILE *f;
+	char str[10];
+	unsigned int k;
+	f = fopen(path, "rb");
+    while (pos--)
+    {
+		fread(&k, 4, 1, f);
+		printf("\nvalue: %u", k);
+    }
+    printf("\n");
+	fclose(f);
 }
