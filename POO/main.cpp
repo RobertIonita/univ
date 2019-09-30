3656054ce5b595bf6e2931380ca1f130328a0823
tonualexandru <tonu.alexandru99@gmail.com>
2019-09-30 10:16:27 +0300

    remove link

--- POO/main.cpp
+++ POO/main.cpp
@@ -5,4 +5,3 @@ int main()
     cout << "Hello, World!";
     return 0;
 }
-https://drive.google.com/drive/folders/1grVvZirBxp-ZsYSscj9UCHGLOx_rKlTj?fbclid=IwAR0EJijsmPzo-uP3ML0OKAogvNNmZZPxS_1PvL5WTlpCCJa84wO-SO9TsKw
