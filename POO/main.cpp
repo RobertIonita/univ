7e05fa10ea7f86cdeeb262f76d790a7b6dee21cf
tonualexandru <tonu.alexandru99@gmail.com>
2019-09-30 10:15:25 +0300

    link

--- POO/main.cpp
+++ POO/main.cpp
@@ -5,3 +5,4 @@ int main()
     cout << "Hello, World!";
     return 0;
 }
+https://drive.google.com/drive/folders/1grVvZirBxp-ZsYSscj9UCHGLOx_rKlTj?fbclid=IwAR0EJijsmPzo-uP3ML0OKAogvNNmZZPxS_1PvL5WTlpCCJa84wO-SO9TsKw
