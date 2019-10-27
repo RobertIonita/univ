cd30422747e52b67fbe16ff5bc34066abef99c2c
tonualexandru <tonu.alexandru99@gmail.com>
2019-10-27 03:30:21 +0300

    idk, anyway

--- 3wquick.c
+++ 3wquick.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #define MAX 20
 
-int arr[] = {4, 9, 4, 4, 1, 9, 4, 4, 9, 4, 4, 1, 4};
+int arr[] = {3, 9, 5, 1, 3},
+    n = sizeof(arr) / sizeof(arr[0]);
 
 void swap(int *a, int *b)
 {
@@ -9,7 +10,7 @@ void swap(int *a, int *b)
     *a = *b;
     *b = t;
 }
-
+void showOff(int n);
 void quickSort(int low, int high)
 {
     int i = low, ci = 0,
@@ -18,48 +19,53 @@ void quickSort(int low, int high)
         val = arr[pivot];
     while (i < j)
     {
-        while (arr[i] <= val  && i <= high)
+        while (arr[i] <= val && i <= high-cj)
         {
             if (arr[i] == val)
-                if (i != ci++)
-                    swap(&arr[ci-1], &arr[i]);
-            if(++i > high) break;
+                if (i-low != ci++)
+                    swap(&arr[low+ci - 1], &arr[i]);
+            if (i + 1 <= high)
+                i++;
+            else
+                break;
         }
-        while (arr[j] >= val && j >= low)
+        while (arr[j] >= val && j >= low+ci)
         {
             if (arr[j] == val)
                 if (high - j != cj++)
-                    swap(&arr[high - cj+1], &arr[j]);
-            if(--j < low) break;
+                    swap(&arr[high - cj + 1], &arr[j]);
+            if (j - 1 >= low)
+                j--;
+            else
+                break;
         }
-        if(i < j) swap(&arr[i], &arr[j]);
+        if (i < j)
+            swap(&arr[i], &arr[j]);
     }
+    showOff(n);
     for (int k = 0; k < ci; k++)
-        swap(&arr[k], &arr[i-k-1]);
+        swap(&arr[low+k], &arr[i - k]);
     for (int k = 0; k < cj; k++)
-        swap(&arr[high-k], &arr[j+k+1]);
-    
-    quickSort(low, j-ci);
-    // quickSort(i+cj, high);
+        swap(&arr[high - k], &arr[j + k]);
 
-    printf("\ni: %d, j: %d, ci: %d, cj: %d", i, j, ci, cj);
-
+    if (low < j - ci)
+        quickSort(low, j - ci);
+    if (i + cj < high)
+        quickSort(i + cj, high);
 }
 
-void showOff(int size)
+void showOff(int n)
 {
     printf("\n");
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
 }
 
 int main()
 {
-    int n;
-    n = sizeof(arr) / sizeof(arr[0]);
     quickSort(0, n - 1);
 
-    showOff(n); //do not uncomment if MAX is greather than 100
+    // showOff(n); //do not uncomment if MAX is greather than 100
 
     return 0;
 }
