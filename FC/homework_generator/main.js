14d4fadc2f8eadbfb510756f66b3ef24e4520632
tonualexandru <tonu.alexandru99@gmail.com>
2019-11-15 10:14:24 +0200

    tema 7

--- FC/homework_generator/main.js
+++ FC/homework_generator/main.js
@@ -8,34 +8,25 @@ window.onload = function () {
         items.innerHTML += `
             <div class="item">
                 <p>Nume și Prenume: <b>${element.innerHTML}</b></p>
-                <p>1)Transformați următoarele numere din baza 2 în baza 16 și în baza 10:</p>
+                <p>Efectuați adunarea în SM, C1 și C2 a numerelor ce urmează:</p>
                 <ol type="a">
                 </ol >
-                <p>2)Transformați următoarele numere din baza 16 în baza 2 și în baza 10:</p>
-                <ol type="a">
-                </ol>
             </div >
         `
         ol = q("ol", qA(".item")[i]);
-        var arr = [],
-            floats = [0.5, 0.25, 0.75, 0.125, 0.625, 0.1875, 0.875, 0.375, 0.3125, 0.0625];
-        while (arr.length < 6) {
-            var r = Math.floor(Math.random() * 100) + 20;
-            if (arr.indexOf(r) === -1) arr.push(r);
-        }
-        for (let j = 0; j < 6; j++) {
-            float = floats[Math.floor(Math.random() * 10)];
-            ol.innerHTML += `<li>${(arr[j] + float).toString(2).toUpperCase()}<sub style="font-size:10px">2</sub></li>`
-        }
-        ol = qA("ol", qA(".item")[i])[1];
-        arr = [];
-        while (arr.length < 6) {
-            var r = Math.floor(Math.random() * 100) + 20;
-            if (arr.indexOf(r) === -1) arr.push(r);
-        }
-        for (let j = 0; j < 6; j++) {
-            float = floats[Math.floor(Math.random() * 10)];
-            ol.innerHTML += `<li>${(arr[j] + float).toString(16).toUpperCase()}<sub style="font-size:10px">16</sub></li>`
+        var arr1 = [],
+            arr2 = [],
+            populate = (array) => {
+                while (array.length < 10) {
+                    var r = Math.floor(Math.random() * 9) + 1;
+                    r = Math.floor(Math.random() * 2) ? "+"+r : "-"+r;
+                    array.push(r);
+                }
+            };
+        populate(arr1);
+        populate(arr2);
+        for (let j = 0; j < 10; j++) {
+            ol.innerHTML += `<li>${arr1[j]} + ${arr2[j]}</li>`
         }
         i++;
     });
