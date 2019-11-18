c96c2531fd724721d0f831998cdc27aebf966e4f
tonualexandru <tonu.alexandru99@gmail.com>
2019-11-18 10:20:14 +0200

    WIP : data gets lost on execution interrupt

--- EA/log.py
+++ EA/log.py
@@ -4,9 +4,21 @@ from datetime import datetime
 sensor = "DH11"
 serial_port = '/dev/cu.wchusbserialfa130'
 baud_rate = 9600
-path = "%s_LOG_%s.txt" % (str(datetime.now()), sensor)
+today = datetime.now()
+today = today.strftime("%d_%m_%Y")
+log_file = "log_"+today+".txt"
 ser = serial.Serial(serial_port, baud_rate)
-with open(path, 'w+') as f:
-    while True:
-        line = ser.readline()
-        f.writelines([line.strip(), " t = %s \n " % (datetime.now())])
+output_file = open(log_file, "w+")
+logs = 0
+while True:
+    line = ser.readline()
+    line = line.decode("utf-8")
+    logs+=1
+    if "Write logs down" in line or logs == 10:
+        print("Oh, write that down")
+        logs = 0
+        output_file.close()
+        output_file = open(log_file, "w+")
+    else:
+        output_file.write(line + str(datetime.now()))
+        print(line)
