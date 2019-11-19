a3d7f837917512d96604694516b0834c9b6ae31d
tonualexandru <tonu.alexandru99@gmail.com>
2019-11-19 11:50:12 +0200

    working

--- EA/log.py
+++ EA/log.py
@@ -6,19 +6,22 @@ serial_port = '/dev/cu.wchusbserialfa130'
 baud_rate = 9600
 today = datetime.now()
 today = today.strftime("%d_%m_%Y")
-log_file = "log_"+today+".txt"
+log_file = "Logs/log_"+today+".txt"
 ser = serial.Serial(serial_port, baud_rate)
 data = []
 while True:
     line = ser.readline()
     line = line.decode("utf-8")
-    if "Write logs down" in line or len(data) == 10:
+    if "Write logs down" in line or len(data) == 5:
         print("Oh, write that down")
-        output_file = open(log_file, "w+")
+        output_file = open(log_file, "a")
         for record in data:
-            output_file.write(record + datetime.now().strftime("%h_%m_%s"))
+            output_file.write(record)
         data=[]
         output_file.close()
     else:
-        data.append(line)
+        time_stamp = datetime.now().strftime("%H:%M:%S")
+        data.append(time_stamp+line)
         print(line)
+
+output_file.close()
