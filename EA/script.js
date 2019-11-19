c010332b619139432b55e81f97a3023cb39a5026
tonualexandru <tonu.alexandru99@gmail.com>
2019-11-19 13:07:04 +0200

    upload logs WIP

---
+++ EA/script.js
@@ -0,0 +1,28 @@
+const LogFolder = './Logs/',
+  fs = require('fs'),
+  readline = require('readline'),
+  fetch = require("node-fetch");
+var makePost = (msg) => {
+  fetch(url, {
+    headers: { 'Content-Type': 'application/json' },
+    method: "POST",
+    body: JSON.stringify({ message: msg})
+  })
+},
+  url = 'http://localhost:5000/api/post';
+
+fs.readdir(LogFolder, (dirReadErr, files) => {
+  if (dirReadErr) throw dirReadErr;
+  files.forEach(file => {
+    if (file.includes("log")) {
+      var rd = readline.createInterface({
+        input: fs.createReadStream(LogFolder + file),
+        output: process.stdout,
+        console: false
+      });
+      rd.on('line', function (line) {
+        makePost(line.toString())
+      });
+    }
+  });
+});
