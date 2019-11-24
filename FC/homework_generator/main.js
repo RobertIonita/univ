5642d01cc5ac4d553f882b6df01fe5ca47b4ca2e
tonualexandru <tonu.alexandru99@gmail.com>
2019-11-24 21:33:20 +0200

    feat: hw8

--- FC/homework_generator/main.js
+++ FC/homework_generator/main.js
@@ -8,7 +8,7 @@ window.onload = function () {
         items.innerHTML += `
             <div class="item">
                 <p>Nume și Prenume: <b>${element.innerHTML}</b></p>
-                <p>Efectuați adunarea în SM, C1 și C2 a numerelor ce urmează:</p>
+                <p>Efectuați adunarea în BCD și E3 a numerelor ce urmează:</p>
                 <ol type="a">
                 </ol >
             </div >
@@ -17,15 +17,15 @@ window.onload = function () {
         var arr1 = [],
             arr2 = [],
             populate = (array) => {
-                while (array.length < 10) {
-                    var r = Math.floor(Math.random() * 9) + 1;
-                    r = Math.floor(Math.random() * 2) ? "+"+r : "-"+r;
+                while (array.length < 6) {
+                    var r = Math.floor(Math.random() * 10000) + 1;
+                    if (r < 1000) r += 1000;
                     array.push(r);
                 }
             };
         populate(arr1);
         populate(arr2);
-        for (let j = 0; j < 10; j++) {
+        for (let j = 0; j < 6; j++) {
             ol.innerHTML += `<li>${arr1[j]} + ${arr2[j]}</li>`
         }
         i++;
