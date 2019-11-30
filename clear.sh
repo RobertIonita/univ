415aa6213bd2a406aadbd186fd90849d0066bc62
tonualexandru <tonu.alexandru99@gmail.com>
2019-12-01 01:41:41 +0200

    prettier and more efective way

--- clear.sh
+++ clear.sh
@@ -1,19 +1,3 @@
-cleanUp() {
-
-    for i in $(ls $1 | grep '.dSYM'); do
-        rm -rd $i
-        i="$1/$i"
-    done
-    for i in $(ls $1); do
-        i="$1/$i"
-        if [ -d "$i" ]; then
-            cleanUp $i
-        else
-            if [[ "$i" == *".exe" ]]; then
-                rm -rf $i
-            fi
-        fi
-    done
-}
-
-cleanUp $1
+find . -iname "*.exe" -exec rm {} \;
+find . -iname "*.dSYM" -exec rm -rd {} \;
+clear
