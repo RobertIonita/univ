84c4d00ee522a743e415eb8d3827e1606d1e6c5b
tonualexandru <tonu.alexandru99@gmail.com>
2019-11-30 22:16:25 +0200

    Base class defined

--- POO/Project/main.cpp
+++ POO/Project/main.cpp
@@ -2,7 +2,44 @@
 #include <fstream>
 #include <string>
 using namespace std;
-int main() {
+
+class Base
+{
+private:
+    string manufacturer,
+        color;
+    int price;
+
+public:
+    virtual void setData(string manufacturer, string color, int price)
+    {
+        this->manufacturer = manufacturer;
+        this->color = color;
+        this->price = price;
+    }
+    virtual void display()
+    {
+        cout << this->manufacturer << endl;
+        cout << this->color << endl;
+        cout << this->price << endl;
+    }
+    virtual Base *getData()
+    {
+        return this;
+    }
+};
+
+int main()
+{
+    Base b,
+        *n;
+    string
+        m = "Google",
+        c = "white";
+    int p = 2;
+    b.setData(m, c, p);
+    n = b.getData();
+    n->display();
 
     return 0;
 }
