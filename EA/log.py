be7036a72e9a44fc222363c11fbb287efcc53704
tonualexandru <tonu.alexandru99@gmail.com>
2019-12-27 23:32:49 +0200

    every 3 services works perfectly on POST requests

--- EA/log.py
+++ EA/log.py
@@ -1,6 +1,8 @@
+import requests
 import serial
 from datetime import datetime
 
+API = "http://192.168.1.100:3000"
 sensor = "DH11"
 serial_port = '/dev/cu.wchusbserialfa130'
 baud_rate = 9600
@@ -9,6 +11,7 @@ today = today.strftime("%d_%m_%Y")
 log_file = "Logs/log_"+today+".txt"
 ser = serial.Serial(serial_port, baud_rate)
 data = []
+log = {}
 while True:
     line = ser.readline()
     line = line.decode("utf-8")
@@ -17,11 +20,33 @@ while True:
         output_file = open(log_file, "a")
         for record in data:
             output_file.write(record)
-        data=[]
+        data = []
         output_file.close()
     else:
         time_stamp = datetime.now().strftime("%H:%M:%S")
-        data.append(time_stamp+line.strip()+"\n")
+        data.append(today+"/"+time_stamp+"-"+line.strip()+"\n")
+        line = line.split("-")
+        log_light = {
+            "date": today,
+            "time": time_stamp,
+            "value": line[0],
+            "set": line[1]
+        }
+        log_temperature = {
+            "date": today,
+            "time": time_stamp,
+            "value": line[2],
+            "set": line[3]
+        }
+        log_water = {
+            "date": today,
+            "time": time_stamp,
+            "value": line[4],
+            "set": line[5]
+        }
+        response_light = requests.post(url=API+"/light", data=log_light)
+        response_temperature = requests.post(url=API+"/temperature", data=log_light)
+        response_water = requests.post(url=API+"/water", data=log_light)
         print(line)
 
 output_file.close()
--- EA/script.js
+++ EA/script.js
@@ -1,15 +1,42 @@
 const LogFolder = './Logs/',
   fs = require('fs'),
   readline = require('readline'),
-  fetch = require("node-fetch");
-var makePost = (msg) => {
-  fetch(url, {
+  fetch = require("node-fetch"),
+  API = "http://192.168.1.100:3000";
+
+var sendLog = (msg) => {
+  msg = msg.split('-');
+  let data_light = {
+    "time": msg[0],
+    "value": msg[1],
+    "set": msg[2]
+  },
+  data_temperature = {
+    "time": msg[0],
+    "value": msg[3],
+    "set": msg[4]
+  },
+  data_water = {
+    "time": msg[0],
+    "value": msg[5],
+    "set": msg[6]
+  }
+  fetch(API+'/light', {
     headers: { 'Content-Type': 'application/json' },
     method: "POST",
-    body: JSON.stringify({ message: msg })
+    body: JSON.stringify(data_light)
+  })
+  fetch(API+'/temperature', {
+    headers: { 'Content-Type': 'application/json' },
+    method: "POST",
+    body: JSON.stringify(data_temperature)
+  })
+  fetch(API+'/water', {
+    headers: { 'Content-Type': 'application/json' },
+    method: "POST",
+    body: JSON.stringify(data_water)
   })
 },
-  url = window.location.hostname+":3000/light",
   rd;
 
 fs.readdir(LogFolder, (dirReadErr, files) => {
@@ -17,12 +44,11 @@ fs.readdir(LogFolder, (dirReadErr, files) => {
   files.forEach(file => {
     if (file.includes("log")) {
       rd = readline.createInterface({
-        input: fs.createReadStream(LogFolder + file),
-        output: process.stdout,
-        console: false
+        input: fs.createReadStream(LogFolder + file)
       });
       rd.on('line', function (line) {
-        makePost(line.toString())
+        console.log(line)
+        sendLog(line.toString())
       });
     }
   });
