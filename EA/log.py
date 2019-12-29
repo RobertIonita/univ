8dcaa9d2084c7a48ac32c841b4e6ca5ff9fe2aab
tonualexandru <tonu.alexandru99@gmail.com>
2019-12-30 00:12:12 +0200

    buggy but mostly works

--- EA/log.py
+++ EA/log.py
@@ -1,52 +1,51 @@
 import requests, serial, time
 from datetime import datetime
 
-API = "http://192.168.1.105:3000"
+API = "http://192.168.0.45:3000"
 sensor = "DH11"
 serial_port = '/dev/cu.wchusbserialfa130'
 baud_rate = 115200
 today = datetime.now()
 today = today.strftime("%d_%m_%Y")
 log_file = "Logs/log_"+today+".txt"
-ser = serial.Serial(serial_port, baud_ratetimeout=.1)
-time.sleep(1) #give the connection a second to settle)
+ser = serial.Serial(serial_port, baud_rate, timeout=.1)
+time.sleep(1)  # give the connection a second to settle)
 data = []
 log = {}
 while True:
-    line = ser.readline()
+    ser.write("123e234e345ef".encode())
+    line = ser.readline().strip()
     line = line.decode("utf-8")
-    if "Write logs down" in line or len(data) == 5:
-        print("Oh, write that down")
-        output_file = open(log_file, "a")
-        for record in data:
-            output_file.write(record)
-        data = []
-        output_file.close()
-    else:
-        time_stamp = datetime.now().strftime("%H:%M:%S")
-        data.append(today+"/"+time_stamp+"-"+line.strip()+"\n")
-        line = line.split("-")
-        log_light = {
-            "date": today,
-            "time": time_stamp,
-            "value": line[0],
-            "set": line[1]
-        }
-        log_temperature = {
-            "date": today,
-            "time": time_stamp,
-            "value": line[2],
-            "set": line[3]
-        }
-        log_water = {
-            "date": today,
-            "time": time_stamp,
-            "value": line[4],
-            "set": line[5]
-        }
-        response_light = requests.post(url=API+"/light", data=log_light)
-        response_temperature = requests.post(url=API+"/temperature", data=log_light)
-        response_water = requests.post(url=API+"/water", data=log_light)
-        print(line)
-
-output_file.close()
+    if line:
+        if len(data) == 10:
+            output_file = open(log_file, "a+")
+            for record in data:
+                output_file.write(record)
+            data = []
+            output_file.close()
+        else:
+            time_stamp = datetime.now().strftime("%H:%M:%S")
+            data.append(today+"/"+time_stamp+"-"+line.strip()+"\n")
+            line = line.split("-")
+            log_light = {
+                "date": today,
+                "time": time_stamp,
+                "value": line[0],
+                "set": line[1]
+            }
+            log_temperature = {
+                "date": today,
+                "time": time_stamp,
+                "value": line[2],
+                "set": line[3]
+            }
+            log_water = {
+                "date": today,
+                "time": time_stamp,
+                "value": line[4],
+                "set": line[5]
+            }
+            rsp_light = requests.post(url=API+"/light", data=log_light)
+            rsp_temperature = requests.post(url=API+"/temperature", data=log_temperature)
+            rsp_water = requests.post(url=API+"/water", data=log_water)
+            print(line, rsp_light.text, rsp_temperature.text, rsp_water.text)
