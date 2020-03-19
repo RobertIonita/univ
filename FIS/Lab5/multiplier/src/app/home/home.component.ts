95895c456c01bc81c1bc5112204eb133e5484c83
tonualexandru <tonu.alexandru99@gmail.com>
2020-03-19 19:51:44 +0200

    static page working fine

---
+++ FIS/Lab5/multiplier/src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, AfterViewInit, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { MatCard } from '@angular/material/card';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+export class HomeComponent implements OnInit {
+
+  homeTitle = 'Workspace';
+  values = 10;
+  multiplier = 5;
+  randomsArr: number[] = [];
+  constructor() {
+    for (let i = 0; i < this.values; i++) {
+      this.randomsArr.push(Math.floor(Math.random() * 100));
+    }
+  }
+  ngOnInit(): void {
+  }
+}
+
+
