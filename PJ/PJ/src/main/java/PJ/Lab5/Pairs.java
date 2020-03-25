9d918810e8f07c9de68eaa9a871e74618f318c83
tonualexandru <tonu.alexandru99@gmail.com>
2020-03-25 21:19:08 +0200

    Ex2 lab 5 finished

---
+++ PJ/PJ/src/main/java/PJ/Lab5/Pairs.java
@@ -0,0 +1,43 @@
+package PJ.Lab5;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Pairs {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        String line = null;
+        int x = 0, y = 0;
+        do {
+            try {
+                System.out.print("Insert first number: ");
+                line = scan.next();
+                x = Integer.parseInt(line);
+                try {
+                    System.out.print("Insert second number: ");
+                    line = scan.next();
+                    y = Integer.parseInt(line);
+                    if (y <= x) {
+                        System.out.println("Invalid input, please insert the pair in ascendent order!\n");
+                        throw new Exception();
+                    }
+                } catch (Exception e) {
+                    continue;
+                }
+            } catch (Exception e) {
+                continue;
+            }
+
+        } while (y <= x);
+        String outputFile;
+        if (args.length > 0)
+            outputFile = args[0];
+        else
+            outputFile = "PJ/PJ/src/main/java/PJ/Lab5/ascending_pair.txt";
+        FileWriter fw = new FileWriter(outputFile);
+        fw.write("the pair is: " + x + "," + y);
+        fw.close();
+        scan.close();
+    }
+}
