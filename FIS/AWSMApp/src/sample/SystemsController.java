464a7ecde9835c9555d3eeb4aa1cecc453ed49dd
tonualexandru <tonu.alexandru99@gmail.com>
2020-04-08 19:08:49 +0300

    Working app with configuration files

---
+++ FIS/AWSMApp/src/sample/SystemsController.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SystemsController {
+    @FXML
+    private void gotoHome(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("homeScene.fxml"));
+        Scene systemsScene = new Scene(root);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(systemsScene);
+        window.show();
+    }
+}
