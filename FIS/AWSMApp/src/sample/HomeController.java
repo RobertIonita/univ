953022b099911a47de8181c34c91bdd379f9d1b0
singularity-bit <maxdsl98@gmail.com>
2020-04-08 20:53:03 +0300

    addButton scene

--- FIS/AWSMApp/src/sample/HomeController.java
+++ FIS/AWSMApp/src/sample/HomeController.java
@@ -20,4 +20,12 @@ public class HomeController {
         window.setScene(systemsScene);
         window.show();
     }
+    @FXML
+    private void  gotoAdd(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("adaugaComponente.fxml"));
+        Scene adaugaComponente= new Scene(root);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(adaugaComponente);
+        window.show();
+    }
 }
