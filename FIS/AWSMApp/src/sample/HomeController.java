5d85dc220762acf6b5f859e2c12ee4f4f0eda4e3
singularity-bit <maxdsl98@gmail.com>
2020-04-11 17:38:16 +0300

    addScene (am impunatatit designul)tipa

--- FIS/AWSMApp/src/sample/HomeController.java
+++ FIS/AWSMApp/src/sample/HomeController.java
@@ -1,17 +1,28 @@
 package sample;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class HomeController {
 
+    @FXML
+    private StackPane parentContainer;
+    @FXML
+    private AnchorPane anchorRoot;
     @FXML
     private void gotoSystems(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("systemsScene.fxml"));
@@ -22,10 +33,16 @@ public class HomeController {
     }
     @FXML
     private void  gotoAdd(ActionEvent event) throws IOException {
+
+
         Parent root = FXMLLoader.load(getClass().getResource("adaugaComponente.fxml"));
-        Scene adaugaComponente= new Scene(root);
+        Scene systemsScene = new Scene(root);
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(adaugaComponente);
+        window.setScene(systemsScene);
         window.show();
+
+
+
     }
+
 }
