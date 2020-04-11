89868f32dc0d74f43ec3c48ad1dc28881759bcab
singularity-bit <maxdsl98@gmail.com>
2020-04-11 13:50:32 +0300

    addScene am impunatatit designul

--- FIS/AWSMApp/src/sample/AddComponentController.java
+++ FIS/AWSMApp/src/sample/AddComponentController.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -10,7 +11,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class AddComponentController {
-
+    @FXML
     private void gotoHome(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("homeScene.fxml"));
         Scene systemsScene = new Scene(root);
@@ -18,4 +19,13 @@ public class AddComponentController {
         window.setScene(systemsScene);
         window.show();
     }
+    @FXML
+    private void gotoSystems(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("systemsScene.fxml"));
+        Scene systemsScene = new Scene(root);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(systemsScene);
+        window.show();
+    }
+
 }
--- FIS/AWSMApp/src/sample/SystemsController.java
+++ FIS/AWSMApp/src/sample/SystemsController.java
@@ -19,4 +19,12 @@ public class SystemsController {
         window.setScene(systemsScene);
         window.show();
     }
+    @FXML
+    private void  gotoAdd(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("adaugaComponente.fxml"));
+        Scene adaugaComponente= new Scene(root);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(adaugaComponente);
+        window.show();
+    }
 }
