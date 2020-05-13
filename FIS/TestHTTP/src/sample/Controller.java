3f66599fc487da81a8f222dd57c51812c6448f10
tonualexandru <tonu.alexandru99@gmail.com>
2020-05-13 19:09:03 +0300

    feat: post request

--- FIS/TestHTTP/src/sample/Controller.java
+++ FIS/TestHTTP/src/sample/Controller.java
@@ -1,26 +1,29 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.Background;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Iterator;
 
 public class Controller {
 
     public StringBuffer jsonStr = new StringBuffer();
+
     public Controller() throws IOException {
-        URL url = new URL("https://tonu.rocks/AWSMApp/api/components.php");
+        URL url = new URL("https://tonu.rocks/school/AWSMApp/api/components.php");
         BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
         String line;
         while ((line = reader.readLine()) != null) {
@@ -47,14 +50,12 @@ public class Controller {
         pane.getChildren().add(name_label);
         pane.getChildren().add(count_label);
         Pane image = new Pane();
-        image.setStyle("-fx-background-image: url("+imgSource+"); -fx-background-size: cover; -fx-background-position: center center;");
+        image.setStyle("-fx-background-image: url(" + imgSource + "); -fx-background-size: cover; -fx-background-position: center center;");
         image.setPrefSize(100, 90);
         image.setLayoutX(0);
         image.setLayoutY(0);
         pane.getChildren().add(image);
         wrapper.getChildren().add(pane);
-        scroll.setContent(wrapper);
-        last.getChildren().add(scroll);
     }
 
     @FXML
@@ -66,7 +67,6 @@ public class Controller {
             String[] elementNames = JSONObject.getNames(objectInArray);
             System.out.printf("%d ELEMENTS IN CURRENT OBJECT:\n", elementNames.length);
             for (String elementName : elementNames) {
-                String k = new Integer(8).toString();
                 System.out.print(elementName + " ");
                 try {
                     System.out.println(objectInArray.getInt(elementName));
@@ -77,10 +77,25 @@ public class Controller {
             String name = objectInArray.getString("name");
             String category = objectInArray.getString("category");
             String imgSource = objectInArray.getString("image");
-            int layoutX = i % 2 == 0 ? 100 : 350;
+            int layoutX = 0;
+
+            if (i % 3 == 1) {
+                layoutX = 340;
+            } else if (i % 3 == 2) {
+                layoutX = 680;
+            }
+
             appendTemplate(name, category, imgSource, layoutX, layoutY);
-            layoutY += i % 2 == 0 ? 0 : 150;
-            System.out.println();
+            layoutY += (i+1) % 3 != 0 ? 0 : 200;
+            System.out.println(layoutX + "Y: " + layoutY + "i: " + i);
         }
+    }
+
+    public void gotoAddComponent(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("AddComponent.fxml"));
+        Scene addComponentScene = new Scene(root);
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setScene(addComponentScene);
+        window.show();
     }
 }
